3f14b77b6a77ec89eea621140d2fcf77f586c949
asm-saim <saim777m@gmail.com>
2026-07-17 15:20:06 +0600

    speed check wwith condition

--- conditional.js
+++ conditional.js
@@ -69,13 +69,19 @@ else {
 }
 // Take a year variable and check whether it is a leap year (hint: year % 4 === 0).
 
-const year = 2030;
-if (year % 4 === 0 && year % 100 != 0 && year % 400 === 0) {
+const year = 2028;
+if (year % 4 === 0 && year % 100 != 0 || year % 400 === 0) {
     console.log("This is leap year")
 }
 else {
     console.log(" Not Leap Year")
 }
 
-
 // Take a speed variable. If speed is greater than 80, print "Overspeeding", otherwise print "Normal speed".
+const speed = 200;
+if (speed > 80) {
+    console.log("Over speed")
+}
+else {
+    console.log("Normal speed")
+}
